d6d22691c328a5d832d5cdb3b81f647be46f6fd6
Isaac Lindegren Ternbom <isaacl@student.chalmers.se>
2023-11-13 16:50:53 +0100

    Add mqtt configurations and implement a mqtt callback funtion that updates the dentist schedule
    Add configMqttClient that reads sensitive mqtt configuration information from a file. Store timeslots booking status in a Map. Add function that loops through and prints time slots. Add hiveconfig to gitignore.

--- DentistTool/src/main/java/ClientMqtt.java
+++ DentistTool/src/main/java/ClientMqtt.java
@@ -1,9 +1,17 @@
 import org.eclipse.paho.client.mqttv3.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 public class ClientMqtt {
 
     public final MqttClient mqttClient;
 
-    public ClientMqtt(String broker, String clientID) throws MqttException {
+    private ClientMqtt(String broker, String clientID) throws MqttException {
         this.mqttClient = new MqttClient(broker, clientID);
     }
 
@@ -16,6 +24,7 @@ public class ClientMqtt {
         options.setPassword(password.toCharArray());
         mqttClient.connect(options);
     }
+
     // Publish function, takes topic and message as argument. Publishes a payload that is converted to a byte array
     public void publish(String topic, String payload) throws MqttException {
         MqttMessage message = new MqttMessage(payload.getBytes());
@@ -30,6 +39,30 @@ public class ClientMqtt {
     // Callback function, used to perform actions whenever a new message arrives.
     public void setCallback(MqttCallback callback) {
         mqttClient.setCallback(callback);
+    }
+
+    // configure mqtt connection
+    public static ClientMqtt configMqttClient() {
+        try (InputStream inputStream = ClientMqtt.class.getClassLoader().getResourceAsStream("hiveconfig.txt")) {
+            if (inputStream == null) {
+                System.out.println("Cannot find hiveconfig.txt in classpath");
+                return null;
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String[] configLines = reader.lines().collect(Collectors.joining("\n")).split("\n");
+
+            String clientId = configLines[0].trim();
+            String brokerURL = configLines[1].trim();
+            String hiveUsername = configLines[2].trim();
+            String hivePassword = configLines[3].trim();
+
+            ClientMqtt clientMqtt = new ClientMqtt(brokerURL, clientId);
+            clientMqtt.connect(hiveUsername, hivePassword);
+            return clientMqtt;
+        } catch (IOException | MqttException e) {
+            System.out.println("Error configuring MQTT client: " + e.getMessage());
+            return null;
+        }
     }
 
 }
--- DentistTool/src/main/java/DentistUI.java
+++ DentistTool/src/main/java/DentistUI.java
@@ -1,5 +1,79 @@
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class DentistUI {
+
+    private static final Map<String, String> timeSlots = new TreeMap<>();   // Use treemap to store the time slots in order
+
     public static void main(String[] args) {
-        System.out.println("Hello dentist");
+        // Instantiate mqtt client
+        ClientMqtt clientMqtt = ClientMqtt.configMqttClient();
+        if (clientMqtt == null) {
+            System.out.println("Failed to configure MQTT client");
+            return;
+        }
+
+        initializeTimeSlots();
+        try {
+            clientMqtt.subscribe("flossboss/timeSlots");
+            clientMqtt.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+                    System.out.println("Connection lost: " + throwable.getMessage());
+                }
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // Split message at ","
+                    String[] messageContent = new String(message.getPayload()).split(",");
+                    String timeSlot = messageContent[0];
+                    String status = messageContent[1];
+                    updateTimeSlot(timeSlot, status);
+                    displayTimeSlots();
+                }
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                }
+            });
+
+        } catch (MqttException exception) {
+            throw new RuntimeException(exception);
+        }
+        // Print time slots for the first time
+        displayTimeSlots();
+
+
+    }   // main loop end
+
+    private static void initializeTimeSlots() {
+        timeSlots.put("08:00", " Free ");
+        timeSlots.put("09:00", " Free ");
+        timeSlots.put("10:00", " Free ");
+        timeSlots.put("11:00", " Free ");
+        timeSlots.put("12:00", " Free ");
+        timeSlots.put("13:00", " Free ");
+        timeSlots.put("14:00", " Free ");
+        timeSlots.put("15:00", " Free ");
+        timeSlots.put("16:00", " Free ");
+        timeSlots.put("17:00", " Free ");
     }
+
+    private static void updateTimeSlot(String timeSlot, String status) {
+        timeSlots.put(timeSlot, status);
+    }
+
+    private static void displayTimeSlots() {
+        System.out.println("\nMY SCHEDULE: \n");
+        for (Map.Entry<String, String> entry : timeSlots.entrySet()) {
+            String timeSlot = entry.getKey();
+            String status = entry.getValue();
+            System.out.println("|| " + timeSlot + " || " + status + " ||" );
+        }
+    }
+
+
+
 }
