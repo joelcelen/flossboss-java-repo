d77877353aff0259d4957add0aa49da47163dc6e
Isaac Lindegren Ternbom <isaacl@student.chalmers.se>
2023-11-13 18:03:13 +0100

    Refactor main loop and add menu for time slots
    Clean main loop by refactoring mqtt callback into its own function. Add menu for time slot options.

--- DentistTool/src/main/java/DentistUI.java
+++ DentistTool/src/main/java/DentistUI.java
@@ -3,6 +3,7 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class DentistUI {
@@ -10,6 +11,7 @@ public class DentistUI {
     private static final Map<String, String> timeSlots = new TreeMap<>();   // Use treemap to store the time slots in order
 
     public static void main(String[] args) {
+
         // Instantiate mqtt client
         ClientMqtt clientMqtt = ClientMqtt.configMqttClient();
         if (clientMqtt == null) {
@@ -18,32 +20,35 @@ public class DentistUI {
         }
 
         initializeTimeSlots();
-        try {
-            clientMqtt.subscribe("flossboss/timeSlots");
-            clientMqtt.setCallback(new MqttCallback() {
-                @Override
-                public void connectionLost(Throwable throwable) {
-                    System.out.println("Connection lost: " + throwable.getMessage());
-                }
-                @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    // Split message at ","
-                    String[] messageContent = new String(message.getPayload()).split(",");
-                    String timeSlot = messageContent[0];
-                    String status = messageContent[1];
-                    updateTimeSlot(timeSlot, status);
-                    displayTimeSlots();
-                }
-                @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                }
-            });
+        mqttCallback(clientMqtt);
 
-        } catch (MqttException exception) {
-            throw new RuntimeException(exception);
+        // TIME SLOT MENU
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        char option;
+
+        while (running) {
+            System.out.println("\n--- DENTIST USER INTERFACE ---\n");
+            System.out.println("Select an option from the menu below: \n");
+            System.out.println("1: View Schedule");
+            System.out.println("2. Add time slots to schedule");
+            System.out.println("3. Remove time slots from schedule");
+            System.out.println("X: Exit \n");
+            option = scanner.next().charAt(0);
+            scanner.nextLine();
+
+            switch (option) {
+                case '1' -> displayTimeSlots();
+                case '2' -> System.out.println("To be implemented");
+                case '3' -> System.out.println("To be implemented");
+                case 'X' | 'x' -> {
+                    running = false;
+                    System.exit(0);
+                }
+            }
         }
-        // Print time slots for the first time
-        displayTimeSlots();
+
 
 
     }   // main loop end
@@ -71,6 +76,33 @@ public class DentistUI {
             String timeSlot = entry.getKey();
             String status = entry.getValue();
             System.out.println("|| " + timeSlot + " || " + status + " ||" );
+        }
+    }
+
+    private static void mqttCallback(ClientMqtt clientMqtt) {
+        try {
+            clientMqtt.subscribe("flossboss/timeSlots");
+            clientMqtt.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+                    System.out.println("Connection lost: " + throwable.getMessage());
+                }
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // Split message at ","
+                    String[] messageContent = new String(message.getPayload()).split(",");
+                    String timeSlot = messageContent[0];
+                    String status = messageContent[1];
+                    updateTimeSlot(timeSlot, status);
+                    displayTimeSlots();
+                }
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                }
+            });
+
+        } catch (MqttException exception) {
+            throw new RuntimeException(exception);
         }
     }
 
