995286b0ea8da43c277708ed4f5b66f62dcf6cfd
joel <joelcel@student.chalmers.se>
2023-11-13 16:44:36 +0100

    add mqtt to appointment-service
    This commit adds the basic connection to the AppointmentService.

--- appointment-service/src/main/java/AppointmentService.java
+++ appointment-service/src/main/java/AppointmentService.java
@@ -1,6 +1,38 @@
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class AppointmentService {
-    public static void main(String[] args) {
-        Appointment appointment = new Appointment();
-        System.out.println(appointment.status());
+    public static void main(String[] args){
+
+        // Instantiate MQTT Broker instance
+        BrokerClient brokerClient = new BrokerClient();
+        brokerClient.connect();
+
+        // Publish payload to topic, placeholder
+        brokerClient.publish("flossboss/test/publish", "I'm the AppointmentService", 0);
+
+        // Subscribe to topic, placeholder
+        brokerClient.subscribe("flossboss/test/subscribe",0);
+
+        // Placeholder callback functionality, replace with real logic once decided
+        brokerClient.setCallback(
+                new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable throwable) {
+                        System.out.println("Connection Lost");
+                    }
+
+                    @Override
+                    public void messageArrived(String topic, MqttMessage mqttMessage) {
+                        System.out.println(mqttMessage);
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                        System.out.println("Delivery Complete");
+                    }
+                }
+        );
     }
 }
