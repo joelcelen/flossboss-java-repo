c6ddb2e0e58e1bec58a627160865c1392b063246
joel <joelcel@student.chalmers.se>
2023-11-13 16:56:29 +0100

    add mqtt to logging-service
    This commit adds the basic client to connect the service to MQTT.

--- logging-service/src/main/java/LoggingService.java
+++ logging-service/src/main/java/LoggingService.java
@@ -1,7 +1,38 @@
-public class LoggingService {
-    public static void main(String[] args) {
-        Logger logger = new Logger();
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-        System.out.println(logger.status());
+public class LoggingService {
+    public static void main(String[] args){
+
+        // Instantiate MQTT Broker instance
+        BrokerClient brokerClient = new BrokerClient();
+        brokerClient.connect();
+
+        // Publish payload to topic, placeholder
+        brokerClient.publish("flossboss/test/publish", "I'm the LoggingService", 0);
+
+        // Subscribe to topic, placeholder
+        brokerClient.subscribe("flossboss/test/subscribe",0);
+
+        // Placeholder callback functionality, replace with real logic once decided
+        brokerClient.setCallback(
+                new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable throwable) {
+                        System.out.println("Connection Lost");
+                    }
+
+                    @Override
+                    public void messageArrived(String topic, MqttMessage mqttMessage) {
+                        System.out.println(mqttMessage);
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                        System.out.println("Delivery Complete");
+                    }
+                }
+        );
     }
 }
