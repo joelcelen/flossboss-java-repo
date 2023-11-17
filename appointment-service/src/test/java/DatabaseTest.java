e4a1c6148ab86bf015e94ea0afce4aae665aa79f
joel <joelcel@student.chalmers.se>
2023-11-17 16:20:15 +0100

    test class for database client
    This commit adds a test class which tests the basic CRUD functionality of the database client. These tests are run with the mvn test command and will be included in the pipeline

---
+++ appointment-service/src/test/java/DatabaseTest.java
@@ -0,0 +1,54 @@
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+
+    private DatabaseClient dbClient;
+    private String itemID;
+
+    /** Creates a client and a test-item before each test **/
+    @Before
+    public void before(){
+        this.dbClient = new DatabaseClient("mongodb+srv://flossboss-test:vaSEAvtHSumlixAv@test-cluster.wlvtb6y.mongodb.net/?retryWrites=true&w=majority");
+        this.dbClient.connect("services-db");
+        this.dbClient.setCollection("services");
+        Document item = new Document()
+                .append("service_name", "AppointmentService_Test")
+                .append("version", 1)
+                .append("status", "testing")
+                .append("available", false);
+        dbClient.createItem(item);
+        this.itemID = dbClient.getID("AppointmentService_Test");
+    }
+
+    /** Deletes the test-item and closes the connection after each test **/
+    @After
+    public void after(){
+        this.dbClient.deleteItem(this.itemID);
+        this.dbClient.disconnect();
+    }
+
+    /** Reads an item and asserts that the JSON retrieved matches the correct item queried for **/
+    @Test
+    public void readItem(){
+        String result = dbClient.readItem(this.itemID);
+        String expected = String.format("{\"_id\": {\"$oid\": \"%s\"}, \"service_name\": \"AppointmentService_Test\", \"version\": 1, \"status\": \"testing\", \"available\": false}", this.itemID);
+        assertEquals(expected, result);
+    }
+
+    /** Fetches an item and updates one of its attributes, then asserts that the changes were made **/
+    @Test
+    public void updateItem(){
+        dbClient.updateItem(this.itemID, "service_name", "AppointmentService_Updated");
+        String result = dbClient.readItem(this.itemID);
+        String expected = String.format("{\"_id\": {\"$oid\": \"%s\"}, \"service_name\": \"AppointmentService_Updated\", \"version\": 1, \"status\": \"testing\", \"available\": false}", this.itemID);
+        assertEquals(expected, result);
+
+        // Change back to the original value since the jobs are not guaranteed to run in order
+        dbClient.updateItem(this.itemID, "service_name", "AppointmentService_Test");
+    }
+}
