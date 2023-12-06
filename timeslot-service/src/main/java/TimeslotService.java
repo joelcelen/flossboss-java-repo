fdb0e177a5151da1ed600de67c578f3698e623a5
joel <joelcel@student.chalmers.se>
2023-12-06 16:23:58 +0100

    logic for handling reconnection
    This commit introduces the logic to reconnect to the broker in case of a lost connection. There is also a custom callback class added to separate concerns further.

--- timeslot-service/src/main/java/TimeslotService.java
+++ timeslot-service/src/main/java/TimeslotService.java
@@ -9,6 +9,7 @@ public class TimeslotService {
         // Instantiate MQTT Broker instance
         BrokerClient brokerClient = BrokerClient.getInstance();
         brokerClient.connect();
+        brokerClient.setCallback(new TimeslotCallback(brokerClient));
 
         // Create Database Client instance
         DatabaseClient databaseClient = DatabaseClient.getInstance();
@@ -19,61 +20,5 @@ public class TimeslotService {
 
         // Set the collection on which you want to operate on
         databaseClient.setCollection(DatabaseCollection.TIMESLOTS.getStringValue());
-
-        // Subscribe to topic, placeholder
-        brokerClient.subscribe(Topic.CLEANUP.getStringValue(), 0);
-        brokerClient.subscribe(Topic.CLINIC.getStringValue(), 0);
-        brokerClient.subscribe(Topic.DENTIST.getStringValue(), 0);
-        brokerClient.subscribe(Topic.ALL.getStringValue(), 0);
-
-        // Create an instance of TimeslotCreator
-        TimeslotCreator timeslotCreator = new TimeslotCreator();
-
-        // Routes payloads to the appropriate methods.
-        brokerClient.setCallback(
-                new MqttCallback() {
-                    @Override
-                    public void connectionLost(Throwable throwable) {
-                        System.out.println("Connection Lost");
-                    }
-
-                    @Override
-                    public void messageArrived(String topic, MqttMessage payload) {
-                        if(topic.equals(Topic.CLEANUP.getStringValue())){
-                            // Takes any payload
-                            timeslotCreator.cleanupTimeslots();
-                        } else if (topic.equals(Topic.CLINIC.getStringValue())) {
-                            // Creates a json-parser that parses the payload to a Java object
-                            Gson parser = new Gson();
-                            Payload message = parser.fromJson(payload.toString(), Payload.class);
-
-                            // Get necessary attributes from Payload
-                            String clinicId = message.getClinicId();
-
-                            timeslotCreator.generateClinic(clinicId);
-
-                        } else if (topic.equals(Topic.DENTIST.getStringValue())) {
-                            // Creates a json-parser that parses the payload to a Java object
-                            Gson parser = new Gson();
-                            Payload message = parser.fromJson(payload.toString(), Payload.class);
-
-                            // Get necessary attributes from Payload
-                            String clinicId = message.getClinicId();
-                            String dentistId = message.getDentistId();
-
-                            timeslotCreator.generateDentist(clinicId, dentistId);
-
-                        } else if (topic.equals(Topic.ALL.getStringValue())) {
-                            // Takes any payload
-                            timeslotCreator.generateAll();
-                        }
-                    }
-
-                    @Override
-                    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-                        System.out.println("Delivery Complete");
-                    }
-                }
-        );
     }
 }
