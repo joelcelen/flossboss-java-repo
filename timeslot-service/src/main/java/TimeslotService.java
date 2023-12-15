665df59700de9cd8613a2ceafab7673d286f8222
joel <joelcel@student.chalmers.se>
2023-12-15 21:35:44 +0100

    restart and shutdown logic timeslotservice
    This commit adds logic to remote restart and shutdown the service via MQTT.

--- timeslot-service/src/main/java/TimeslotService.java
+++ timeslot-service/src/main/java/TimeslotService.java
@@ -1,24 +1,25 @@
-import com.google.gson.Gson;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-
 public class TimeslotService {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // Instantiate MQTT Broker instance
         BrokerClient brokerClient = BrokerClient.getInstance();
         brokerClient.connect();
-        brokerClient.setCallback(new TimeslotCallback(brokerClient));
 
         // Create Database Client instance
         DatabaseClient databaseClient = DatabaseClient.getInstance();
 
-        //TODO:
-        // - Change this database to the actual database when integrating the service.
         databaseClient.connect("flossboss");
 
         // Set the collection on which you want to operate on
         databaseClient.setCollection(DatabaseCollection.TIMESLOTS.getStringValue());
+
+        // Create ShutdownManager
+        ShutdownManager shutdownManager = new ShutdownManager();
+
+        // Listen to topics while shutdown is not requested
+        while (!ShutdownManager.shutdownRequested){
+            brokerClient.setCallback(new TimeslotCallback(brokerClient));
+        }
+        shutdownManager.shutdown();
     }
 }
--- timeslot-service/src/main/java/Topic.java
+++ timeslot-service/src/main/java/Topic.java
@@ -3,7 +3,9 @@ public enum Topic {
     CLEANUP("flossboss/timeslots/cleanup"),
     CLINIC("flossboss/timeslots/clinic"),
     DENTIST("flossboss/timeslots/dentist"),
-    ALL("flossboss/timeslots/all");
+    ALL("flossboss/timeslots/all"),
+    SHUTDOWN("flossboss/shutdown/timeslot"),
+    RESTART("flossboss/restart/timeslot");
 
     private final String stringValue;
 
